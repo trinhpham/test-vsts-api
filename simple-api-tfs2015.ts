0276da4dfeecccfe833e2f3f0af7604c0547cf77
Trinh Pham <trinh.pham@logigear.com>
2018-08-29 08:55:13 +0700

    Remove unused import

--- simple-api-tfs2015.ts
+++ simple-api-tfs2015.ts
@@ -1,8 +1,8 @@
 import { IRequestHandler } from "vso-node-api/interfaces/common/VsoBaseInterfaces";
-import { TestPlan, TestPoint, TestCaseResult, TestRun } from "vso-node-api/interfaces/TestInterfaces";
-import { RunCreateModel, RunUpdateModel } from "vso-node-api/interfaces/TestInterfaces";
-import { WebApi, getHandlerFromToken, getBasicHandler } from "vso-node-api/WebApi";
+import { TestPlan } from "vso-node-api/interfaces/TestInterfaces";
+import { WebApi, getBasicHandler } from "vso-node-api/WebApi";
 import { TestApi } from "vso-node-api/TestApi";
+import { ConnectionData } from "vso-node-api/interfaces/LocationsInterfaces";
 
 const DATA_TEST = {
     enpoitUrl: "http://localhost:8080/tfs/DefaultCollection",
@@ -12,14 +12,14 @@ const DATA_TEST = {
 }
 
 async function main(){
-    const credHandler = getBasicHandler(DATA_TEST.usr, DATA_TEST.pwd);
+    const credHandler: IRequestHandler = getBasicHandler(DATA_TEST.usr, DATA_TEST.pwd);
     const webApi: WebApi = new WebApi(DATA_TEST.enpoitUrl, credHandler);
-    const connectionData = await webApi.connect();
+    const connectionData: ConnectionData = await webApi.connect();
     console.log("Connected to WebApi: " + connectionData.instanceId);
     const testApi: TestApi = await webApi.getTestApi();
     console.log("Passed get TestApi");
     const plans: TestPlan[] = await testApi.getPlans(DATA_TEST.project);
-    plans.forEach((plan) => {
+    plans.forEach((plan: TestPlan) => {
         console.log(`Found test plan ${plan.name}`);
     });
 }
